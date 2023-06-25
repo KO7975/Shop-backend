b9507fd169d53d098c62d996c71a183de3f4c79d
KO7975 <kiosya17@gmail.com>
2023-06-25 19:39:20 +0300

    swager dockumentation for register view changed

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,24 +1,17 @@
-from django.shortcuts import render
 from django.conf import settings
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.hashers import check_password, make_password
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView 
-from rest_framework.exceptions import AuthenticationFailed
 from .models import User
 from .serializer import UserSerializer
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.permissions import AllowAny
 import jwt
 import smtplib
 from email.message import EmailMessage
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
-from rest_framework.parsers import JSONParser
-from .serializer import MyTokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse
 
-    
+
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
@@ -36,14 +29,12 @@ def send_email_with_token(token, recipient_email):
     smtp_password = settings.EMAIL_HOST_PASSWORD
 
     message = EmailMessage()
-    message['Subject'] = 'Подтверждение аутентификации'
+    message['Subject'] = 'Підтвердженя аутентифікації PINI PINO'
     message['From'] = smtp_username
     message['To'] = recipient_email
 
-    # Тело сообщения с токеном
-    message.set_content(f'Для подтверждения аутентификации перейдите по ссылке: http://127.0.0.1:8000/auth/register/apruve?token={token}&email={recipient_email}')
+    message.set_content(f'Для підтвердженря аутентифікації перейдіть за посиланням: http://127.0.0.1:8000/auth/register/apruve?token={token}&email={recipient_email}')
 
-    # Отправка сообщения 
     with smtplib.SMTP(smtp_server, smtp_port) as server:
         server.starttls()
         server.login(smtp_username, smtp_password)
@@ -51,11 +42,22 @@ def send_email_with_token(token, recipient_email):
 
 
 
-
 class RegisterView(APIView):
-
     permission_classes = (AllowAny,)
 
+    @extend_schema(
+        request=UserSerializer,
+        methods=["POST"],
+        parameters=[
+            OpenApiParameter(name='email', description='Email adress', required=True),
+            OpenApiParameter(name='password', description='Password', required=True),
+            ],
+        description="Registration form.\
+        Takes a set of user credentials and sent message to user email adress\
+        with refresh JSON web token to prove the authentication of those credentials.",
+        responses={200: OpenApiResponse(description='Registration saccess. User get email with attached link for access'),
+                   201: OpenApiResponse(description='Email allready in db')}
+    )
     def post(self, request):
         data = request.data
         email = data['email']
@@ -65,7 +67,7 @@ class RegisterView(APIView):
         if len(data2)>0:
             data = data2
 
-        if len(User.objects.filter(email=email)) == 0:
+        if not User.objects.filter(email=email).exists():
             data['is_active'] = False
             user = User.objects.create_user(**data)
             token = get_tokens_for_user(user)
@@ -74,15 +76,23 @@ class RegisterView(APIView):
 
             return Response({'message': 'Registration saccess. Check mail.'})
         
-        return Response({'message': 'Email allready in db'})
+        return Response({'message': 'Email allready in db'}, status=201)
+    
 
-
-
-class RegistrApruveView(APIView):
-    permission_classes = (AllowAny,)
-
+    @extend_schema(
+            request={'token':'refresh', 'email': 'email@example.com'},
+            description='User mail upproved from email. Make user is_active = True',
+            methods=["GET"],
+            parameters=[
+                OpenApiParameter(name='refresh', description='Token', required=True),
+                OpenApiParameter(name='email', description='Email adress', required=True),
+            ],
+            responses={200:OpenApiResponse( description='return token: refresh, access',),
+                       400:OpenApiResponse(description='Token not valid'),
+                       500:OpenApiResponse(description='UserDoesNotExist')
+                       }
+    )
     def get(self, request):
-
         token = request.GET.get('token')
         email = request.GET.get('email' )
 
@@ -107,7 +117,7 @@ class RegistrApruveView(APIView):
         except User.DoesNotExist as e:
             user = User.objects.get(email = email)
             user.delete()
-            return Response({'message': f'{e}'})
+            return Response({'message': f'{e}'}, status=500)
 
 
 
@@ -134,107 +144,6 @@ class RegistrApruveView(APIView):
 #             return Response(get_tokens_for_user(user))
 #         else:
 #             return Response({'error': 'Invalid token'}, status=400)
-        
-
-
-
-
-# class VerifyView(APIView):
-#     def get(self, request):
-#         token = request.GET.get('token')
-#         try:
-#             refresh = RefreshToken(token)
-#             access = refresh.access_token
-#             data = {
-#                 "refresh" :str(refresh),
-#                 "access": str(access)
-#                 }
-#             # Перенаправление пользователя на страницу подтверждения аутентификации с access token
-#             return Response({'message': 'Valid access', 'token': access}, data=data)
-
-#         except Exception as e:
-#             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class LoginView(APIView):
-#     permission_classes = (AllowAny,)
-
-#     def post(self, request):
-#         email = request.data.get('email')
-#         password = request.data.get('password')
-#         user =User.objects.filter(email=email).first()
-
-#         if user is None:
-#             raise AuthenticationFailed('User not found')
-#         # login(request, user)
-
-#         if  check_password(password, user.password) or  password==user.password:
-#             token = RefreshToken.for_user(user)
-#             token['id'] = user.id
-#             token['email'] = user.email
-#             access = token.access_token
-#             response = Response()
-#             response.data = {
-#                 'message': 'login saccess',
-#                 'refresh': str(token),
-#                 'access': str(access),
-#             }
-#             response.set_cookie(key='token', value=access)
-#             return response
-        
-#         raise AuthenticationFailed('Incorrect password')
-
-
-# class LogoutView(APIView):
-#     permission_classes = (IsAuthenticated,)
-#     def post(self,request):
-#         response = Response()
-#         response.delete_cookie("token")
-#         response.data = {
-#             "message": "Logout success"
-#         }
-#         return response
-    
-
-# class GetUser(APIView):
-#     # permission_classes = (IsAuthenticated,)
-
-#     def get(self, request):
-    
-#         user = User.objects.filter(email=request.data['email']).first()
-#         serializer = UserSerializer(user)
-#         data = serializer.data
-
-#         return Response({"data":data})
-    
-
-#     def put(self, request):   
-
-#         user  = User.objects.get(email=request.data['email']).first()
-#         serializer = UserSerializer(user, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=400)
-    
-
-#     def delete(self, request):        
-#         user = User.objects.filter(email=request.data['email'])
-#         if user is None:
-#             return Response({"message": "User not found"})
-#         user.delete()
-#         return Response({"message":"User deleted saccess"}, status=status.HTTP_204_NO_CONTENT)
-
-    
-# class GetUsers(APIView):
-#     # permission_classes = (IsAuthenticatedOrReadOnly,)
-
-#     def get(self, request):
-#         token = request.COOKIES.get('token')
-#         if not token:
-#             raise AuthenticationFailed("Unauthenticated")
-#         users = User.objects.all().values()
-#         return Response({"users": list(users)})
         
 
 
